92f139df29af5c88ff945f93319a31b1cd17b8b4
Elan van Biljon <elan@vanbiljon.co.za>
2017-07-17 17:01:28 +0200

    Add basic body parsing to find info

--- main.js
+++ main.js
@@ -8,19 +8,28 @@ var pagePrefix = "http://www.creepypasta.com/page/";
 var pageNumber = 1;
 
 var url = pagePrefix + pageNumber.toString();
-request(url, function(error, response, body) {
-   if(error) {
-     console.log("Error: " + error);
-   }
+// request(url, function(error, response, body) {
+//    if(error) {
+//      console.log("Error: " + error);
+//    }
+//
+//    if(response.statusCode === 200) {
+//      // Parse the document body
+//      var $ = cheerio.load(body);
+//      console.log("Page title:  " + $('title').text());
+// 	 fs.writeFile(pageNumber.toString() + ".txt", body, function(err) {
+// 		 if(err) throw err;
+// 	 });
+//    }
+// });
 
-   if(response.statusCode === 200) {
-     // Parse the document body
-     var $ = cheerio.load(body);
-     console.log("Page title:  " + $('title').text());
-	 fs.writeFile(pageNumber.toString() + ".txt", body, function(err) {
-		 if(err) {
-			 console.log(err);
-		 }
-	 });
-   }
+fs.readFile(pageNumber.toString() + ".txt", "utf8", function(err, data) {
+	if(err) throw err;
+
+	var $ = cheerio.load(data);
+	var $posts = $('div.post-content h3.post-title a');
+	console.log($posts.length);
+	$posts.each(function(index, element) {
+		console.log($(this).attr('title'));
+	});
 });
