e33187bd3cad8973536dbe4c9c159784f6b95e8e
Elan van Biljon <elan@vanbiljon.co.za>
2017-07-17 12:55:18 +0200

    Text basic request and file write

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 var parallel = require('async-parallel');
 var request = require('request');
-var $ = require('cheerio');
+var cheerio = require('cheerio');
 var URL = require('url-parse');
 var fs = require('file-system');
+
+var pagePrefix = "http://www.creepypasta.com/page/";
+var pageNumber = 1;
+
+var url = pagePrefix + pageNumber.toString();
+request(url, function(error, response, body) {
+   if(error) {
+     console.log("Error: " + error);
+   }
+
+   if(response.statusCode === 200) {
+     // Parse the document body
+     var $ = cheerio.load(body);
+     console.log("Page title:  " + $('title').text());
+	 fs.writeFile(pageNumber.toString() + ".txt", body, function(err) {
+		 if(err) {
+			 console.log(err);
+		 }
+	 });
+   }
+});
