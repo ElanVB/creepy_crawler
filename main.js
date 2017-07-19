ca7f9fd476026b51f6742dc630f4ca8efe6ce30b
Elan van Biljon <elan@vanbiljon.co.za>
2017-07-19 02:28:55 +0200

    Avoid too many requests problem

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var cheerio = require('cheerio');
 var URL = require('url-parse');
 var fs = require('file-system');
 
-var posts = []
+var posts = [];
 
 var pagePrefix = "http://www.creepypasta.com/page/";
 var pageNumber = 0;
@@ -26,7 +26,7 @@ function getStopPage(after) {
 		if(response.statusCode === 200) {
 			var $ = cheerio.load(body);
 			endPageNumber = parseInt($('title').text().toLowerCase().split(" ")[5]);
-			
+
 			after();
 		} else {
 			console.log("Status Code: ", response.statusCode);
@@ -76,29 +76,6 @@ function getPageInfo(url, pushToPosts, afterEach) {
 	});
 }
 
-// function fetchPostInfo(pushToPosts) {
-// 	fs.readFile(pageNumber.toString() + ".txt", "utf8", function(err, data) {
-// 		if(err) throw err;
-//
-// 		var $ = cheerio.load(data);
-// 		var $posts = $('div.post-content');
-//
-// 		$posts.each(function(index, element) {
-// 			$post = $(this);
-// 			$postTitleLink = $post.find('h3.post-title a');
-// 			$postRatingInfo = $post.find('div.gdrts-rating-text');
-// 			$postRating = $postRatingInfo.children('strong');
-//
-// 			pushToPosts({
-// 				'title': $postTitleLink.attr('title'),
-// 				'url': $postTitleLink.attr('href'),
-// 				'rating': parseFloat($postRating.text()),
-// 				'votes': parseInt($postRatingInfo.text().split(" ")[15])
-// 			});
-// 		});
-// 	});
-// }
-
 function pushToPosts(post) {
 	posts.push(post);
 }
@@ -113,16 +90,19 @@ function savePostsWhenDone() {
 	}
 }
 
+var pageIncrement = 0;
 function getPages() {
-	for(var pageIncrement = 0; pageIncrement + pageNumber <= endPageNumber; pageIncrement++) {
+	if(pageIncrement + pageNumber <= endPageNumber) {
 		var url = pagePrefix + (pageNumber + pageIncrement).toString();
 		startCount++;
+		pageIncrement++;
 		getPageInfo(url, pushToPosts, function() {
 			endCount++;
 		});
+		setTimeout(getPages, 500);
+	} else {
+		savePostsWhenDone();
 	}
-
-	savePostsWhenDone();
 }
 
 getStopPage(getPages);
