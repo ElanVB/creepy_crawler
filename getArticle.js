af9d693cf3a09442147559ae9a1f310db40ab3ce
Elan van Biljon <elan@vanbiljon.co.za>
2017-07-19 02:39:29 +0200

    Save pasta if above threshhold

--- getArticle.js
+++ getArticle.js
@@ -5,31 +5,62 @@ var URL = require('url-parse');
 var fs = require('file-system');
 
 var article = [];
+const THRESHHOLD = 9.0;
 
 function pushToArticle(paragraph) {
 	article.push(paragraph);
 }
 
-function loadPostInfo(pushToArticle) {
+function printArticle() {
+	console.log(article.join('\n'));
+}
+
+function saveArticle(name) {
+	fs.writeFile('pastas/' + name + '.txt', article.join('\n'), function(err) {
+		if(err) throw err;
+	});
+}
+
+function loadPostInfo(pushToArticle, after) {
 	fs.readFile("postData.json", "utf8", function(err, data) {
 		if(err) throw err;
 
 		var posts = JSON.parse(data);
 
-		for(var i in posts) {
+		var i = 0;
+		// for(var i in posts) {
 			var url = posts[i]['url'];
 			var name = url.split('/')[3];
+			var rating = posts[i]['rating'];
 
-			// var $ = cheerio.load(data);
-			// var $paragraphs = $('div.single-content div.clearfix p');
-			//
-			// $paragraphs.each(function(index, element) {
-			// 	$paragraph = $(this);
-			//
-			// 	pushToPosts($paragraph.text());
-			// });
-		}	
+			if(rating >= THRESHHOLD) {
+				request({
+					method: 'GET',
+					url: url,
+					timeout: 10000
+				}, function(error, response, body) {
+					if(error) {
+						console.log("Error: " + error);
+					}
+
+					if(response.statusCode === 200) {
+						var $ = cheerio.load(body);
+						var $paragraphs = $('div.single-content div.clearfix p');
+
+						$paragraphs.each(function(index, element) {
+							$paragraph = $(this);
+							pushToArticle($paragraph.text());
+						});
+
+						after(name);
+					} else {
+						console.log("Status Code: ", response.statusCode);
+					}
+				});
+			}
+		// }
 	});
 }
 
-loadPostInfo(pushToArticle);
+fs.mkdir('pastas');
+loadPostInfo(pushToArticle, saveArticle);
