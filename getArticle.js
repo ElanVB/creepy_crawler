a227a9585fbbe04a717e168ee35d9f7dc19706b3
Elan van Biljon <elan@vanbiljon.co.za>
2017-07-19 03:02:02 +0200

    Complete get Articles

--- getArticle.js
+++ getArticle.js
@@ -4,63 +4,84 @@ var cheerio = require('cheerio');
 var URL = require('url-parse');
 var fs = require('file-system');
 
-var article = [];
 const THRESHHOLD = 9.0;
-
-function pushToArticle(paragraph) {
-	article.push(paragraph);
-}
+var queue = [];
 
 function printArticle() {
 	console.log(article.join('\n'));
 }
 
-function saveArticle(name) {
+function saveArticle(name, article) {
 	fs.writeFile('pastas/' + name + '.txt', article.join('\n'), function(err) {
 		if(err) throw err;
 	});
 }
 
-function loadPostInfo(pushToArticle, after) {
+function addToQueue(name, url) {
+	queue.push({
+		'name': name,
+		'url': url
+	});
+}
+
+function fetchArticle(name, url, saveArticle) {
+	request({
+		method: 'GET',
+		url: url,
+		timeout: 10000
+	}, function(error, response, body) {
+		if(error) {
+			console.log("Error: " + error);
+		}
+
+		if(response.statusCode === 200) {
+			var $ = cheerio.load(body);
+			var $paragraphs = $('div.single-content div.clearfix p');
+			var article = [];
+			
+			$paragraphs.each(function(index, element) {
+				$paragraph = $(this);
+				article.push($paragraph.text());
+			});
+
+			saveArticle(name, article);
+		} else {
+			console.log("Status Code: ", response.statusCode);
+		}
+	});
+}
+
+var count = 0;
+function fetchArticles() {
+	if(count < queue.length) {
+		fetchArticle(queue[count]['name'], queue[count]['url'], saveArticle);
+		count++;
+		setTimeout(fetchArticles, 500);
+	}
+}
+
+function loadPostInfo(addToQueue, fetchArticles, print) {
 	fs.readFile("postData.json", "utf8", function(err, data) {
 		if(err) throw err;
 
 		var posts = JSON.parse(data);
 
-		var i = 0;
-		// for(var i in posts) {
+		for(var i in posts) {
 			var url = posts[i]['url'];
 			var name = url.split('/')[3];
 			var rating = posts[i]['rating'];
 
 			if(rating >= THRESHHOLD) {
-				request({
-					method: 'GET',
-					url: url,
-					timeout: 10000
-				}, function(error, response, body) {
-					if(error) {
-						console.log("Error: " + error);
-					}
-
-					if(response.statusCode === 200) {
-						var $ = cheerio.load(body);
-						var $paragraphs = $('div.single-content div.clearfix p');
-
-						$paragraphs.each(function(index, element) {
-							$paragraph = $(this);
-							pushToArticle($paragraph.text());
-						});
-
-						after(name);
-					} else {
-						console.log("Status Code: ", response.statusCode);
-					}
-				});
+				addToQueue(name, url);
 			}
-		// }
+		}
+
+		print();
+		fetchArticles();
 	});
 }
 
 fs.mkdir('pastas');
-loadPostInfo(pushToArticle, saveArticle);
+loadPostInfo(addToQueue, fetchArticles, function() {
+	console.log(queue.length);
+});
